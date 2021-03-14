f7a652ec8d43d5aeb82eda922dadd15185483af2
Sebastian Mertins <sebastian@sem-ba.de>
2021-03-14 16:48:11 +0100

    Added basic style for index and loaded svg via ajax

--- web-app/public/index/functions.js
+++ web-app/public/index/functions.js
@@ -1,3 +1,28 @@
+// Event which is triggered when document is ready
+$('document').ready(() => {
+    loadSVG();
+})
+
+/**
+ * Load SVG via ajax and insert it into container
+ * Used for preventing long svg inline element
+ */
+function loadSVG() {
+    $.ajax({
+        type: 'GET',
+        url: 'index/map_germany.svg',
+        data: '',
+        dataType: 'html',
+        success: function (data) {
+            $('#svgContainer').html(data);
+        }
+    });
+}
+
+/**
+ * Click method for trigger action on state click
+ * @param iso String of source state
+ */
 function clickOnState(iso){
     console.log(iso);
 }
