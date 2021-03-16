064a9cb361eca91aba6d12f3ea174de691dde3a1
Sebastian Mertins <sebastian@sem-ba.de>
2021-03-16 11:02:53 +0100

    Enable start deployment of linux or mac

--- manage-k8s.sh
+++ manage-k8s.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-# Start Kubernetes in minikube from scratch.
-# Launch minikube and add needed helm charts
-# Call start function
-function setup(){
+
+# Launch minikube from a mac system and start setup of minikube
+function kubernetesMac(){
   unset KUBECONFIG
   minikube start --memory 8192 --cpus 4 --vm-driver=hyperkit
+  setup
+}
+
+# Launch minikube from a linux system and start setup of minikube
+function kubernetesLinux(){
+  unset KUBECONFIG
+  minikube start --memory 6144 --cpus 4 --vm-driver=hyperkit
+  setup
+}
+
+# Setup basic for kubernetes in minikube
+function setup(){
   minikube addons enable ingress
   helm repo add strimzi http://strimzi.io/charts/
   helm repo add stable https://charts.helm.sh/stable
 
-  start
-}
-
-# Install and start strimzi kafka cluster
-# Install and start hadoop cluster
-function start(){
+  # Install kafka and strimzi cluster
   helm install my-kafka-operator strimzi/strimzi-kafka-operator
   kubectl apply -f kafka-cluster.yaml
   helm install --namespace=default --set hdfs.dataNode.replicas=1 --set yarn.nodeManager.replicas=1 --set hdfs.webhdfs.enabled=true my-hadoop-cluster stable/hadoop
-
-  # Not used -> Caused errors on kafka cluster. Better run manual after everything is up and running
-  # skaffold dev
 }
+
 
 # Clear the whole minikube to get a clean base
 function clear() {
