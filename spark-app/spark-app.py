f4c15ee3fb5d4f684cca377898f57be4195bb2df
PSeegers97 <p.seegers87@gmail.com>
2021-03-30 07:39:00 -0700

    deleted comments

--- spark-app/spark-app.py
+++ spark-app/spark-app.py
@@ -36,8 +36,6 @@ kafkaMessages = spark \
 
 # Example Part 3
 # Convert value: binary -> JSON -> fields + parsed timestamp
-""" trackingMessages = kafkaMessages.select(
-    # Extract 'value' from Kafka message (i.e., the tracking data)
     from_json(
         column("value").cast("string"),
         trackingMessageSchema
@@ -56,55 +54,32 @@ kafkaMessages = spark \
 
 # Example Part 4
 # Compute most popular slides
-""" popular = trackingMessages.groupBy(
-    window(
-        column("parsed_timestamp"),
-        windowDuration,
-        slidingDuration
-    ),
-    column("mission")
-).count().withColumnRenamed('count', 'views') """
 
 # Example Part 5
 # Start running the query; print running counts to the console
-""" consoleDump = popular \
     .writeStream \
     .trigger(processingTime=slidingDuration) \
     .outputMode("update") \
     .format("console") \
     .option("truncate", "false") \
-    .start() """
-
-# Example Part 6
 
 
-""" def saveToDatabase(batchDataframe, batchId):
+
     # Define function to save a dataframe to mysql
     def save_to_db(iterator):
         # Connect to database and use schema
         session = mysqlx.get_session(dbOptions)
-        session.sql("USE popular").execute()
-
         for row in iterator:
             # Run upsert (insert or update existing)
-            sql = session.sql("INSERT INTO popular "
-                              "(mission, count) VALUES (?, ?) "
-                              "ON DUPLICATE KEY UPDATE count=?")
-            sql.bind(row.mission, row.views, row.views).execute()
-
         session.close()
 
     # Perform batch UPSERTS per data partition
-    batchDataframe.foreachPartition(save_to_db) """
 
 # Example Part 7
 
 
-""" dbInsertStream = popular.writeStream \
     .trigger(processingTime=slidingDuration) \
     .outputMode("update") \
-    .foreachBatch(saveToDatabase) \
-    .start() """
 
 # Wait for termination
 spark.streams.awaitAnyTermination()
