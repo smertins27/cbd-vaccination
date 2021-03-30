0707dd30cc6e535228e066b3017110552c60808f
Tom <tomseevers@outlook.com>
2021-03-30 02:53:00 -0700

    all in one, leider
    :(

--- spark-app/spark-app.py
+++ spark-app/spark-app.py
@@ -1,12 +1,12 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.sql.types import IntegerType, StringType, StructType, TimestampType
+from pyspark.sql.types import IntegerType, StringType, StructType, TimestampType, FloatType, LongType, DecimalType
 import mysqlx
 
 dbOptions = {"host": "my-app-mysql-service", 'port': 33060, "user": "root", "password": "mysecretpw"}
 dbSchema = 'popular'
 windowDuration = '5 minutes'
-slidingDuration = '1 minute'
+slidingDuration = '3 minutes'
 
 # Example Part 1
 # Create a spark session
@@ -28,10 +28,19 @@ kafkaMessages = spark \
     .load()
 
 # Define schema of tracking data
-""" trackingMessageSchema = StructType() \
+"""  trackingMessageSchema = StructType() \
     .add("mission", StringType()) \
     .add("timestamp", IntegerType()) """
-
+  
+trackingVaccinationsSchema = StructType() \
+    .add("statesiso", StringType()) \
+    .add("vac_amount", LongType()) \
+    .add("vaccinescode", StringType()) \
+    .add("timestamp", IntegerType())  \
+    .add("percent", FloatType())  \
+    .add("vacId", IntegerType())   \
+    .add("progressId", IntegerType())    
+    # .add("percentage", FloatType())    
 # Example Part 3
 # Convert value: binary -> JSON -> fields + parsed timestamp
 """ trackingMessages = kafkaMessages.select(
@@ -52,6 +61,29 @@ kafkaMessages = spark \
     .withColumnRenamed('json.mission', 'mission') \
     .withWatermark("parsed_timestamp", windowDuration) """
 
+
+trackingVaccination = kafkaMessages.select(
+    from_json(
+        column("value").cast("string"),
+        trackingVaccinationsSchema
+    ).alias("json")
+).select(
+    from_unixtime(column('json.timestamp'))
+    .cast(TimestampType())
+    .alias("parsed_timestamp"),
+
+    column("json.*")
+) \
+    .withColumnRenamed('json.statesiso', 'statesiso') \
+    .withColumnRenamed('json.vac_amount', 'vac_amount') \
+    .withColumnRenamed('json.vaccinescode', 'vaccinescode') \
+    .withColumnRenamed('json.percent', 'percent') \
+    .withColumnRenamed('json.progressId', 'progressId') \
+    .withColumnRenamed('json.vacId', 'vacId') \
+    .withWatermark("parsed_timestamp", windowDuration)
+
+    
+
 # Example Part 4
 # Compute most popular slides
 """ popular = trackingMessages.groupBy(
@@ -63,6 +95,35 @@ kafkaMessages = spark \
     column("mission")
 ).count().withColumnRenamed('count', 'views') """
 
+vaccinationsProgress = trackingVaccination.groupBy(
+    window(
+        column("parsed_timestamp"),
+        windowDuration,
+        slidingDuration
+    ),
+    column("progressId"),
+    column("statesiso"),
+    column("vaccinescode")
+    
+    
+).agg(sum('percent').alias('percentage'))
+
+
+print(vaccinationsProgress)
+
+vaccinations = trackingVaccination.groupBy(
+    window(
+        column("parsed_timestamp"),
+        windowDuration,
+        slidingDuration
+    ),
+    column("vacId"),
+    column("vaccinescode"),
+    column("statesiso")
+).agg(sum('vac_amount').alias('vac_amount'), avg('vac_amount').alias('vac_amount_average'))
+
+
+
 # Example Part 5
 # Start running the query; print running counts to the console
 """ consoleDump = popular \
@@ -73,7 +134,60 @@ kafkaMessages = spark \
     .option("truncate", "false") \
     .start() """
 
-# Example Part 6
+consoleVaccinationsDumb = vaccinations \
+    .writeStream \
+    .trigger(processingTime=slidingDuration) \
+    .outputMode("update") \
+    .format("console") \
+    .option("truncate", "false") \
+    .start()
+
+ 
+
+consoleVaccinationsProgressDumb = vaccinationsProgress \
+    .writeStream \
+    .trigger(processingTime=slidingDuration) \
+    .outputMode("update") \
+    .format("console") \
+    .option("truncate", "false") \
+    .start()               
+
+# Save to Vaccinations
+
+
+def saveToVaccinationsDatabase(batchDataframe, batchId):
+    # Define function to save a dataframe to mysql
+
+    def save_to_db(iterator):
+        
+        # Connect to database and use schema
+        session = mysqlx.get_session(dbOptions)
+        session.sql("USE vaccination").execute()
+        for row in iterator:
+            print(row)
+            # Run upsert (insert or update existing)
+            if hasattr(row, 'vacId'):
+                print(row.vacId)
+                print(row.vaccinescode)
+                print(row.statesiso)
+                print(row.vac_amount)
+                print(row.vac_amount_average)
+                sql = session.sql("INSERT INTO vaccinations (id, vaccinescode, statesiso, vac_amount, vac_amount_average) VALUES ( ?, ?, ?, ?) ON DUPLICATE KEY UPDATE vac_amount = ?, vac_amount_average = ?")
+                sql.bind(row.vacId, row.vaccinescode, row.statesiso, row.vac_amount, row.vac_amount_average, row.vac_amount).execute()
+            
+    
+            elif hasattr(row, 'progressId'):
+                print("vaccination_progress")
+                print(row.progressId)
+                sql = session.sql("INSERT INTO vaccination_progress (id, percentage, statesiso, vaccinescode) VALUES ( ?, ?, ?) ON DUPLICATE KEY UPDATE percentage = ?")
+                sql.bind(row.progessId, row.percentage, row.statesiso, row.vaccinescode, row.percentage)
+        session.close()
+
+    # Perform batch UPSERTS per data partition
+    batchDataframe.foreachPartition(save_to_db) 
+
+ 
+    # Save to vaccination progress
 
 
 """ def saveToDatabase(batchDataframe, batchId):
@@ -81,7 +195,7 @@ kafkaMessages = spark \
     def save_to_db(iterator):
         # Connect to database and use schema
         session = mysqlx.get_session(dbOptions)
-        session.sql("USE popular").execute()
+        session.sql("USE vaccinations").execute()
 
         for row in iterator:
             # Run upsert (insert or update existing)
@@ -103,6 +217,18 @@ kafkaMessages = spark \
     .outputMode("update") \
     .foreachBatch(saveToDatabase) \
     .start() """
+
+vaccinationInsertStream = vaccinations.writeStream \
+    .trigger(processingTime=slidingDuration) \
+    .outputMode("update") \
+    .foreachBatch(saveToVaccinationsDatabase) \
+    .start() 
+
+vaccinationsProgressInsertStream = vaccinationsProgress.writeStream \
+    .trigger(processingTime=slidingDuration) \
+    .outputMode("update") \
+    .foreachBatch(saveToVaccinationsDatabase) \
+    .start() 
 
 # Wait for termination
 spark.streams.awaitAnyTermination()
